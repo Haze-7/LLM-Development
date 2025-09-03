d6f42bc6cb2541c05ea4fb62187461e8375a779f
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-09-03 12:17:59 -0500

    Pseudocode Writeup

---
+++ Chiasson_csc4700_cshw1.py
@@ -0,0 +1,14 @@
+#LLM Devlopment HW 1:
+
+# from collections import OrderedDict
+
+#Pseudo Code: 
+
+#1. Read text file provided 
+#2. Tokenize it with .split (identify each word) / w/ for loop
+#3. add ^ into ordered set (no dupes, indexes) / key: value pair w/ index: value (string)
+#4. return the set / set is tokens
+#5.  Do probablility / trigram / bigram formuala to calc frequency. 
+#   Higher the freq, goes first, then in order
+
+
