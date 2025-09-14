f4a42937e8bdb280f7e5eba1a3498a9f95223b54
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-09-14 13:58:48 -0500

    comment shortening

--- hw1/Chiasson_csc4700_cshw1.py
+++ hw1/Chiasson_csc4700_cshw1.py
@@ -163,22 +163,22 @@ def main() :
   #b. An argument (--data) that points to the path of training corpus.
   parser.add_argument("--data", help = "Points to path of training corpus.")   
 
-  #c. An argument (--save) that points to the path where the ngram or BPE model will be saved so that it can be loaded in 
+  #c. An argument (--save) that points to the path where the ngram or BPE model was saved
   parser.add_argument("--save", help = "Path to where Model is saved to be loaded.")
 
   #d. An argument (--load) that points to the path where the trained ngram model was saved.
   parser.add_argument("--load", help = "Path to where trained ngram model is saved.")
 
-  #e. A string argument (--word) that specifies the first word (or words) used for the “predict_ngram” activity.
+  #e. A string argument (--word) that specifies the first word (or words) used to predict.
   parser.add_argument("--word", nargs='+', help = "Specifies the first word(or words) for the Prediction activity.")
 
-  #f. An integer argument (--nwords) that specifies the number of words to predict for the “predict_ngram” activity.
+  #f. An integer argument (--nwords) that specifies the number of words to predict.
   parser.add_argument("--nwords", type = int, help = "Specifies the number of words to predict for the Prediction activity.")
 
   #g. An integer arugment (--n) that specifies the order of the ngram (choices should be 2 or 3). 
   parser.add_argument("--n", type = int, choices = [2, 3], help = "Select Order of the Ngram 2 (bi) or 3 (tri).")
 
-  #h. An argument (--d) that set the deterministic flag for the predict_next_word() methodof of the ngram model to True.
+  #h. An argument (--d) that set the deterministic flag to True.
   parser.add_argument("--d", action = "store_true", help = "Set the deterministic flag for Prediction Model.")
 
   #Activities Handling:
