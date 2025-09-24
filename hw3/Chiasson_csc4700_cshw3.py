57b393b257a0cc36629ca4040c89a6bf11e5a039
Hayes Chiasson <hchias2@lsu.edu>
2025-09-24 12:09:26 -0500

    gitignore, prog 3 setup

---
+++ hw3/Chiasson_csc4700_cshw3.py
@@ -0,0 +1,57 @@
+"""
+LLM Development HW 3:
+
+Project Description
+
+"""
+from dotenv import load_dotenv
+import pydantic
+
+
+"""
+
+Libraries we can use:
+- openai
+- python-dotenv
+-pydantic
+
+
+Instructions
+1. Get dev set
+- extract 1st 500 questions / answers
+- skip is_impossible questions
+
+2. w/ api Run GPT-5 Nano in batch mode 
+- use minimal reasoning_effort
+- generate answers to all questions from step 1
+- test w. small batch first (3 or 4 lines)
+
+Question 1: What prompt did I use?
+
+3. w/ Api, run qwen / quen3-8b (serially / not batch) 
+- 1 question after another
+- generate answers to all questions from step 1 (dev set)
+
+Question 2: What prompt did I use?
+
+4. w/ api, use gpt-5-mini in batch mode to score responses of previous 2
+- mark as either True(correct) or False(incorrect)
+- provide an explanation
+- use STRUCTURED OUTPUTS to ensure vlaid output JSON
+- save outputs for each model in Json file that can be reused
+- files should be named like (gpt-5-nanp-DATE-hw3.json)
+- use structured_outputs feature to get both
+    - explanation(str)
+    -score (boolean)
+Use prompt shown belowL
+
+Question 3: What was the total accuracy of GPT-5 nano?
+Question 4: What was the total accuracy of quwen/quwn3-8b
+Question 5: What are insights about the results (3-4 sentences)
+
+Scoring Prompt:
+
+At bottom of INstructions
+
+
+"""
